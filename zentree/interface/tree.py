34b313b9582262d0530940bb12e020ef220dc041
Nikolaus Gullotta <nikolaus.gullotta@protonmail.com>
2021-11-28 21:15:42 -0600

    Get parallax in working orde

--- zentree/interface/tree.py
+++ zentree/interface/tree.py
@@ -1,4 +1,5 @@
 from enum import IntEnum, auto
+from operator import delitem
 from pathlib import Path
 from random import choice, randint, uniform
 from typing import List
@@ -44,7 +45,12 @@ class Arboretum(Pager):
             with open(Path(f"./trees/{_type}.txt")) as fp:
                 self.tree = fp.read().split("\n")
 
-        for i, line in enumerate(self.tree):
+        tcopy = self.tree.copy()
+        for i in range(len(self.values) - 1, -1, -1):
+            try:
+                line: str = tcopy[-1]
+            except (IndexError):
+                continue
             new: List[str] = []
             for bc, tc in zip(
                 list(self.values[i]), list(line.center(self.width - 1))
@@ -52,12 +58,13 @@ class Arboretum(Pager):
                 new.append(bc)
                 if tc != " ":
                     new[-1] = tc
+            del tcopy[-1]
             self.values[i] = "".join(new)
 
     def advanceSpecials(self) -> None:
         # Special objects specify which row it's in, it's start and end
         # coordinates, and the original string value
-        for row, coords, original in self.specials:
+        for row, coords, original, special in self.specials:
             # The actual string "row" in the pager
             string: str = self.values[row]
 
@@ -65,23 +72,30 @@ class Arboretum(Pager):
             start: int = self.clamp(coords[0], 0, len(string))
             end: int = self.clamp(coords[1], 0, len(string))
 
-            # This special object is no longer being drawn, delete it
-            if start == 0 and end == 0:
-                idx: int = self.specials.index((row, coords, original))
-                del self.specials[idx]
-                continue
-
             # First we decompose this string into a list of chars
             decomposed: List[str] = list(string)
 
             # Shift special objects down the list by one and then set
             # the end char back to the original string's char
             if start > 0:
-                decomposed[start - 1 : end - 1] = decomposed[start:end]
+                decomposed[start - 1 : end] = list(special)
                 decomposed[end] = original[end]
-            else:
+            # Shift the special object down in sequential order by
+            # inversing end coord and then setting end char back to
+            # original end char
+            elif end > 0:
+                print(0, end)
+                decomposed[0:end] = list(special)[-end:]
+                decomposed[end] = original[end]
+            # We're done here, this shouldn't be rendered anymore,
+            # delete it and move on
+            elif start == 0 and end == 0:
+                # This special object is no longer being drawn, delete it
                 decomposed[0] = original[0]
-                decomposed[end] = original[end]
+                idx: int = self.specials.index(
+                    (row, coords, original, special)
+                )
+                del self.specials[idx]
 
             # Decrement coordinates
             coords[0] -= 1
@@ -97,7 +111,7 @@ class Arboretum(Pager):
         if len(self.specials) >= 5:
             return
 
-        if self.chance(35):
+        if self.chance(15):
             special: str = choice(self.special)
 
             s: str = choice(self.values)
@@ -108,7 +122,7 @@ class Arboretum(Pager):
             if idx + 1 == len(self.values):
                 no_spawn = True
 
-            for row, _, __ in self.specials:
+            for row, _, __, ___ in self.specials:
                 if row == idx:
                     no_spawn = True
 
@@ -123,6 +137,7 @@ class Arboretum(Pager):
                         idx,
                         [start, (start + len(special) - 1), Direction.RIGHT],
                         cs,
+                        special,
                     )
                 )
 
